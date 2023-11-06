7786a364ffc815403bacdee309f6ff8b7174f820
xiaolou86 <20718693+xiaolou86@users.noreply.github.com>
2023-11-06 16:46:25 +0800

    fix typos

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,4 +1,4 @@
-// Tags that are used to seperate the different parts of the script
+// Tags that are used to separate the different parts of the script
 const ROLLUP_NAME_TAG: &[u8] = &[1];
 const SIGNATURE_TAG: &[u8] = &[2];
 const PUBLICKEY_TAG: &[u8] = &[3];
